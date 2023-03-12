2d40e6e8a11496e9831dbeaec713c323b9632e06
calvinadhikang <calvinadhikang@gmail.com>
2023-03-12 20:59:35 +0700

    add API Libary and Database Script

--- application/views/test.php
+++ application/views/test.php
@@ -8,5 +8,24 @@
 </head>
 <body>
 	<h1>Ini Test</h1>
+<?php
+// $conn = oci_connect('kp', 'kp', 'localhost/XE');
+// if (!$conn) {
+//     $e = oci_error();
+//     trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+// }
+
+// $stid = oci_parse($conn, 'INSERT INTO TEST VALUES(:val1)');
+// $value = "INSERTED FROM PHP";
+// oci_bind_by_name($stid, ':val1', $value);
+// oci_execute($stid);
+
+// $r = oci_parse($conn, 'SELECT * FROM TEST');
+// oci_execute($r);
+
+// $nrows = oci_fetch_all($r, $res);
+
+// var_dump($res);
+?>
 </body>
 </html>
