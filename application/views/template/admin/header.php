c84a643184da20521a00dfa547de9759e6fd4ca5
ivanderbw <ivanderkw2@gmail.com>
2023-04-01 20:38:48 +0700

    icon

--- application/views/template/admin/header.php
+++ application/views/template/admin/header.php
@@ -118,7 +118,7 @@
 								<a class="nav-link " href="<?= base_url() ?>">
 									<div
 										class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
-										<i class="ni ni-world-2 text-danger text-sm opacity-10"></i>
+										<i class="fa fa-sign-out text-danger text-sm opacity-10"></i>
 									</div>
 									<span class="nav-link-text ms-1">Logout</span>
 								</a>
